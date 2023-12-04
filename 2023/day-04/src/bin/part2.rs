53c876cf3c27c85c6e94b9ec141c6c3c57a7af58
Mauro Sambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2023-12-04 14:34:36 -0300

    day 4 part 2

---
+++ 2023/day-04/src/bin/part2.rs
@@ -0,0 +1,58 @@
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let input = include_str!("../../input.txt");
+
+    let result = process(input)?;
+
+    println!("{result}");
+
+    Ok(())
+}
+
+#[inline]
+fn process(input: &str) -> Result<u32> {
+    let cards = day_04::parse_cards(input)?;
+    let mut card_quantity = vec![1; cards.len()];
+
+    let result = cards
+        .into_iter()
+        .enumerate()
+        .map(|(idx, c)| {
+            let winning_count = c
+                .numbers_you_have
+                .into_iter()
+                .filter(|n| c.winning_numbers.contains(n))
+                .count();
+
+            let current_card_quantity = card_quantity[idx];
+
+            for i in idx + 1..=idx + winning_count {
+                card_quantity[i] += current_card_quantity;
+            }
+
+            card_quantity[idx]
+        })
+        .sum();
+
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        let result = process(input).unwrap();
+
+        assert_eq!(30, result);
+    }
+}
