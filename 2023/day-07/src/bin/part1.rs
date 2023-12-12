774e46826da17e14e75a8fdd7aa99e9464ec453c
Mauro Sambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2023-12-12 00:28:39 -0300

    day 7 migrate structs to traits for reuse

--- 2023/day-07/src/bin/part1.rs
+++ 2023/day-07/src/bin/part1.rs
@@ -1,4 +1,7 @@
-use anyhow::Result;
+use day_07::{Card, HandType};
+
+use anyhow::{bail, Result};
+use itertools::Itertools;
 
 fn main() -> Result<()> {
     let input = include_str!("../../input.txt");
@@ -12,7 +15,7 @@ fn main() -> Result<()> {
 
 #[inline]
 fn process(input: &str) -> Result<u64> {
-    let hands = day_07::parse_camel_cards(input)?;
+    let hands = day_07::parse_camel_cards::<CamelCard, CamelHandType>(input)?;
 
     let result = hands
         .into_values()
@@ -21,6 +24,73 @@ fn process(input: &str) -> Result<u64> {
         .sum();
 
     Ok(result)
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum CamelCard {
+    C2,
+    C3,
+    C4,
+    C5,
+    C6,
+    C7,
+    C8,
+    C9,
+    T,
+    J,
+    Q,
+    K,
+    A,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum CamelHandType {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+impl Card for CamelCard {
+    fn from_char(c: char) -> Result<Self> {
+        Ok(match c {
+            '2' => CamelCard::C2,
+            '3' => CamelCard::C3,
+            '4' => CamelCard::C4,
+            '5' => CamelCard::C5,
+            '6' => CamelCard::C6,
+            '7' => CamelCard::C7,
+            '8' => CamelCard::C8,
+            '9' => CamelCard::C9,
+            'T' => CamelCard::T,
+            'J' => CamelCard::J,
+            'Q' => CamelCard::Q,
+            'K' => CamelCard::K,
+            'A' => CamelCard::A,
+            _ => bail!("Character is not a card"),
+        })
+    }
+}
+
+impl HandType<CamelCard> for CamelHandType {
+    fn from_cards(hand: &[CamelCard; 5]) -> Self {
+        let counts = hand.iter().counts();
+        let counts = counts.values().sorted().collect_vec();
+
+        match counts[..] {
+            [5] => CamelHandType::FiveOfAKind,
+            [1, 4] => CamelHandType::FourOfAKind,
+            [2, 3] => CamelHandType::FullHouse,
+            [1, 1, 3] => CamelHandType::ThreeOfAKind,
+            [1, 2, 2] => CamelHandType::TwoPair,
+            [1, 1, 1, 2] => CamelHandType::OnePair,
+            [1, 1, 1, 1, 1] => CamelHandType::HighCard,
+            _ => unreachable!("Unexpected array {counts:?}",),
+        }
+    }
 }
 
 #[cfg(test)]
--- 2023/day-07/src/lib.rs
+++ 2023/day-07/src/lib.rs
@@ -1,55 +1,51 @@
-use std::cmp::{Ordering, Reverse};
+use std::cmp::Ordering;
 use std::collections::BTreeMap;
+use std::hash::Hash;
 use std::str::FromStr;
 
-use anyhow::{bail, Context, Result};
-use itertools::Itertools;
+use anyhow::{Context, Result};
 
 #[derive(Debug)]
-pub struct CamelHand {
-    pub cards: [CamelCard; 5],
-    pub hand_type: CamelHandType,
+pub struct CamelHand<C: Card, T: HandType<C>> {
+    pub cards: [C; 5],
+    pub hand_type: T,
 }
 
-impl FromStr for CamelHand {
+pub trait HandType<C: Card>: Ord {
+    fn from_cards(hand: &[C; 5]) -> Self;
+}
+
+pub trait Card: Ord + Hash + Sized {
+    fn from_char(c: char) -> Result<Self>;
+}
+
+impl<C: Card, T: HandType<C>> FromStr for CamelHand<C, T> {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let cards: [CamelCard; 5] = s
+        let cards: [C; 5] = s
             .chars()
-            .map(TryInto::<CamelCard>::try_into)
-            .collect::<Result<Vec<CamelCard>>>()?
+            .map(C::from_char)
+            .collect::<Result<Vec<C>>>()?
             .try_into()
             .ok()
             .context("String must have exaclty 5 cards")?;
 
-        let counts = cards.iter().counts();
-        let counts = counts.values().sorted_by_key(|&n| Reverse(n)).collect_vec();
-
-        let hand_type = match counts[..] {
-            [5] => CamelHandType::FiveOfAKind,
-            [4, 1] => CamelHandType::FourOfAKind,
-            [3, 2] => CamelHandType::FullHouse,
-            [3, 1, 1] => CamelHandType::ThreeOfAKind,
-            [2, 2, 1] => CamelHandType::TwoPair,
-            [2, 1, 1, 1] => CamelHandType::OnePair,
-            [1, 1, 1, 1, 1] => CamelHandType::HighCard,
-            _ => unreachable!("Unexpected array {counts:?}",),
-        };
+        let hand_type = T::from_cards(&cards);
 
         Ok(CamelHand { cards, hand_type })
     }
 }
 
-impl PartialEq for CamelHand {
+impl<C: Card, T: HandType<C>> PartialEq for CamelHand<C, T> {
     fn eq(&self, other: &Self) -> bool {
         self.cards == other.cards
     }
 }
 
-impl Eq for CamelHand {}
+impl<C: Card, T: HandType<C>> Eq for CamelHand<C, T> {}
 
-impl Ord for CamelHand {
+impl<C: Card, T: HandType<C>> Ord for CamelHand<C, T> {
     fn cmp(&self, other: &Self) -> Ordering {
         self.hand_type
             .cmp(&other.hand_type)
@@ -57,73 +53,24 @@ impl Ord for CamelHand {
     }
 }
 
-impl PartialOrd for CamelHand {
+impl<C: Card, T: HandType<C>> PartialOrd for CamelHand<C, T> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
-pub enum CamelCard {
-    C2,
-    C3,
-    C4,
-    C5,
-    C6,
-    C7,
-    C8,
-    C9,
-    T,
-    J,
-    Q,
-    K,
-    A,
-}
-
-impl TryFrom<char> for CamelCard {
-    type Error = anyhow::Error;
-
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        Ok(match value {
-            '2' => CamelCard::C2,
-            '3' => CamelCard::C3,
-            '4' => CamelCard::C4,
-            '5' => CamelCard::C5,
-            '6' => CamelCard::C6,
-            '7' => CamelCard::C7,
-            '8' => CamelCard::C8,
-            '9' => CamelCard::C9,
-            'T' => CamelCard::T,
-            'J' => CamelCard::J,
-            'Q' => CamelCard::Q,
-            'K' => CamelCard::K,
-            'A' => CamelCard::A,
-            _ => bail!("Character is not a card"),
-        })
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub enum CamelHandType {
-    HighCard,
-    OnePair,
-    TwoPair,
-    ThreeOfAKind,
-    FullHouse,
-    FourOfAKind,
-    FiveOfAKind,
-}
-
-pub fn parse_camel_cards(input: &str) -> Result<BTreeMap<CamelHand, u32>> {
+pub fn parse_camel_cards<C: Card, T: HandType<C>>(
+    input: &str,
+) -> Result<BTreeMap<CamelHand<C, T>, u32>> {
     input.lines().map(parse_camel_hand).collect()
 }
 
-fn parse_camel_hand(input: &str) -> Result<(CamelHand, u32)> {
+fn parse_camel_hand<C: Card, T: HandType<C>>(input: &str) -> Result<(CamelHand<C, T>, u32)> {
     let (hand, bid) = input
         .split_once(' ')
         .context("Hand and Bid must be space separated")?;
 
-    let hand = hand.parse::<CamelHand>()?;
+    let hand = hand.parse::<CamelHand<C, T>>()?;
     let bid = bid.parse::<u32>()?;
 
     Ok((hand, bid))
