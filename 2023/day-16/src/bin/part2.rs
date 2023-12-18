66c59dfa12a09f567d3628b72fcb0e3f43ab1956
Mauro Sambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2023-12-18 01:49:48 -0300

    day 16 rayon to make it faster

--- 2023/day-16/src/bin/part2.rs
+++ 2023/day-16/src/bin/part2.rs
@@ -1,7 +1,8 @@
 use std::collections::HashSet;
 
 use anyhow::{Context, Result};
-use day_16::Direction;
+use day_16::{Contraption, Direction};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 fn main() -> Result<()> {
     let input = include_str!("../../input.txt");
@@ -17,39 +18,41 @@ fn main() -> Result<()> {
 fn process(input: &str) -> Result<u64> {
     let contraption = day_16::parse_contraption(input)?;
 
-    let result = (0..contraption.length)
-        .map(|i| {
-            contraption
-                .clone()
-                .energize(Direction::Down, Some((i, 0)))
-                .collect::<HashSet<_>>()
-                .len() as u64
-        })
-        .chain((0..contraption.length).map(|i| {
-            contraption
-                .clone()
-                .energize(Direction::Up, Some((i, contraption.height - 1)))
-                .collect::<HashSet<_>>()
-                .len() as u64
-        }))
-        .chain((0..contraption.height).map(|i| {
-            contraption
-                .clone()
-                .energize(Direction::Right, Some((0, i)))
-                .collect::<HashSet<_>>()
-                .len() as u64
-        }))
-        .chain((0..contraption.height).map(|i| {
-            contraption
-                .clone()
-                .energize(Direction::Left, Some((contraption.length - 1, i)))
-                .collect::<HashSet<_>>()
-                .len() as u64
-        }))
+    let length = contraption.length;
+    let height = contraption.height;
+
+    let result = (0..length)
+        .into_par_iter()
+        .map(|i| test_ray(&contraption, Direction::Down, (i, 0)))
+        .chain(
+            (0..height)
+                .into_par_iter()
+                .map(|i| test_ray(&contraption, Direction::Right, (0, i))),
+        )
+        .chain(
+            (0..length)
+                .into_par_iter()
+                .map(|i| test_ray(&contraption, Direction::Up, (i, height - 1))),
+        )
+        .chain(
+            (0..height)
+                .into_par_iter()
+                .map(|i| test_ray(&contraption, Direction::Left, (length - 1, i))),
+        )
         .max()
         .context("Expect at least one")?;
 
     Ok(result)
+}
+
+#[inline]
+#[must_use]
+fn test_ray(contraption: &Contraption, direction: Direction, position: (usize, usize)) -> u64 {
+    contraption
+        .clone()
+        .energize(direction, Some(position))
+        .collect::<HashSet<_>>()
+        .len() as u64
 }
 
 #[cfg(test)]
