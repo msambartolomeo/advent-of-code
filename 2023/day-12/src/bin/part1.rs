700b0be9de9c2fb57239758c0d1c69371915ba95
Mauro Sambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2024-01-05 16:19:43 -0300

    move posibilities calculation to lib

--- 2023/day-12/src/bin/part1.rs
+++ 2023/day-12/src/bin/part1.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use day_12::{Record, Spring};
 use itertools::Itertools;
 
 fn main() -> Result<()> {
@@ -16,55 +15,10 @@ fn main() -> Result<()> {
 fn process(input: &str) -> Result<u32> {
     let records = day_12::parse_spring_records(input);
 
-    let result = records.process_results(|it| it.map(|r| unknown_spring_posibilities(&r)).sum())?;
+    let result =
+        records.process_results(|it| it.map(|r| day_12::unknown_spring_posibilities(&r)).sum())?;
 
     Ok(result)
-}
-
-pub fn unknown_spring_posibilities(record: &Record) -> u32 {
-    unknown_spring_posibilities_rec(&record.0, &record.1, 0)
-}
-
-fn unknown_spring_posibilities_rec(
-    springs: &[Spring],
-    damaged_groups: &[u32],
-    damaged_count: u32,
-) -> u32 {
-    match springs.split_first() {
-        Some((Spring::Operational, springs)) => {
-            if damaged_groups.first() == Some(&damaged_count) {
-                unknown_spring_posibilities_rec(springs, &damaged_groups[1..], 0)
-            } else if damaged_count == 0 {
-                unknown_spring_posibilities_rec(springs, damaged_groups, 0)
-            } else {
-                0
-            }
-        }
-        Some((Spring::Damaged, springs)) => match damaged_groups.first() {
-            None => 0,
-            Some(&group) if group == damaged_count => 0,
-            _ => unknown_spring_posibilities_rec(springs, damaged_groups, damaged_count + 1),
-        },
-        Some((Spring::Unknown, _)) => {
-            // Divide into damaged and operational posibilities
-            let mut springs = springs.to_vec();
-
-            springs[0] = Spring::Damaged;
-            let damaged_posibilities =
-                unknown_spring_posibilities_rec(&springs, damaged_groups, damaged_count);
-
-            springs[0] = Spring::Operational;
-
-            let operational_posibilities =
-                unknown_spring_posibilities_rec(&springs, damaged_groups, damaged_count);
-
-            damaged_posibilities + operational_posibilities
-        }
-        None => u32::from(
-            damaged_groups.is_empty()
-                || (damaged_groups.len() == 1 && damaged_groups[0] == damaged_count),
-        ),
-    }
 }
 
 #[cfg(test)]
@@ -97,7 +51,7 @@ mod tests {
     fn test_each(#[case] input: &str, #[case] expected: u32) -> Result<()> {
         let record = day_12::parse_record(input)?;
 
-        let result = unknown_spring_posibilities(record);
+        let result = day_12::unknown_spring_posibilities(&record);
 
         assert_eq!(expected, result);
 
--- 2023/day-12/src/lib.rs
+++ 2023/day-12/src/lib.rs
@@ -22,6 +22,52 @@ impl TryFrom<char> for Spring {
 
 pub type Record = (Vec<Spring>, Vec<u32>);
 
+pub fn unknown_spring_posibilities(record: &Record) -> u32 {
+    unknown_spring_posibilities_rec(&record.0, &record.1, 0)
+}
+
+fn unknown_spring_posibilities_rec(
+    springs: &[Spring],
+    damaged_groups: &[u32],
+    damaged_count: u32,
+) -> u32 {
+    match springs.split_first() {
+        Some((Spring::Operational, springs)) => {
+            if damaged_groups.first() == Some(&damaged_count) {
+                unknown_spring_posibilities_rec(springs, &damaged_groups[1..], 0)
+            } else if damaged_count == 0 {
+                unknown_spring_posibilities_rec(springs, damaged_groups, 0)
+            } else {
+                0
+            }
+        }
+        Some((Spring::Damaged, springs)) => match damaged_groups.first() {
+            None => 0,
+            Some(&group) if group == damaged_count => 0,
+            _ => unknown_spring_posibilities_rec(springs, damaged_groups, damaged_count + 1),
+        },
+        Some((Spring::Unknown, _)) => {
+            // Divide into damaged and operational posibilities
+            let mut springs = springs.to_vec();
+
+            springs[0] = Spring::Damaged;
+            let damaged_posibilities =
+                unknown_spring_posibilities_rec(&springs, damaged_groups, damaged_count);
+
+            springs[0] = Spring::Operational;
+
+            let operational_posibilities =
+                unknown_spring_posibilities_rec(&springs, damaged_groups, damaged_count);
+
+            damaged_posibilities + operational_posibilities
+        }
+        None => u32::from(
+            damaged_groups.is_empty()
+                || (damaged_groups.len() == 1 && damaged_groups[0] == damaged_count),
+        ),
+    }
+}
+
 pub fn parse_spring_records(input: &str) -> impl Iterator<Item = Result<Record>> + '_ {
     input.lines().map(parse_record)
 }
