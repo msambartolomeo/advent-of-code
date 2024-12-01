68341f2689efcaa198c2868ceb62c78081b833c0
Mauro Sambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2024-12-01 15:51:20 -0300

    day-01 part2

--- 2024/day-01/src/bin/part2.rs
+++ 2024/day-01/src/bin/part2.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use itertools::Itertools;
 
 fn main() -> Result<()> {
     let input = include_str!("../../input.txt");
@@ -12,9 +13,21 @@ fn main() -> Result<()> {
 
 #[inline]
 fn process(input: &str) -> Result<u64> {
-    let _input = day_01::parser::parse(input)?;
+    let (hl1, hl2) = day_01::parser::parse(input)?;
 
-    todo!()
+    let counts1 = hl1.into_iter().map(|n| n.0).counts();
+    let counts2 = hl2.into_iter().map(|n| n.0).counts();
+
+    let result = counts1
+        .into_iter()
+        .map(|(id, count)| {
+            let appearences = counts2.get(&id).copied().unwrap_or_default();
+
+            id * appearences as u64 * count as u64
+        })
+        .sum();
+
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -22,12 +35,20 @@ mod tests {
     #![allow(unused)]
     use super::*;
 
+    const INPUT: &str = "
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+
     #[test]
     fn test_example() -> Result<()> {
-        let expected: u64 = todo!();
-        let input = todo!();
+        let expected: u64 = 31;
 
-        let result = process(input)?;
+        let result = process(INPUT)?;
 
         assert_eq!(expected, result);
 
