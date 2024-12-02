fc3746f3663ff9ff55efc1409995475b5d1e8347
Mauro Sambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2024-12-02 11:00:48 -0300

    day-02 part1

--- 2024/day-02/src/bin/part1.rs
+++ 2024/day-02/src/bin/part1.rs
@@ -1,4 +1,7 @@
+use std::cmp::Ordering;
+
 use anyhow::Result;
+use itertools::Itertools;
 
 fn main() -> Result<()> {
     let input = include_str!("../../input.txt");
@@ -11,8 +14,34 @@ fn main() -> Result<()> {
 }
 
 #[inline]
-fn process(_input: &str) -> Result<u64> {
-    todo!()
+fn process(input: &str) -> Result<u64> {
+    let reports = day_02::parser::parse(input);
+
+    let result = reports
+        .filter_ok(|v| is_safe(v))
+        .process_results(|it| it.count())? as u64;
+
+    Ok(result)
+}
+
+fn is_safe(v: &[u64]) -> bool {
+    let first = v[0];
+
+    v.iter()
+        .skip(1)
+        .try_fold((first, Ordering::Equal), |(last, ordering), &next| {
+            let new_ord = next.cmp(&last);
+            let diff = last.abs_diff(next);
+
+            if diff == 0 || diff > 3 {
+                Err(())
+            } else if matches!(ordering, Ordering::Equal) || new_ord == ordering {
+                Ok((next, new_ord))
+            } else {
+                Err(())
+            }
+        })
+        .is_ok()
 }
 
 #[cfg(test)]
@@ -21,13 +50,19 @@ mod tests {
     use super::*;
 
     const INPUT: &str = "
-";
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
+    ";
 
     #[test]
     fn test_example() -> Result<()> {
-        let expected: u64 = todo!();
+        let expected: u64 = 2;
 
-        let result = process(INPUT)?;
+        let result = process(INPUT.trim())?;
 
         assert_eq!(expected, result);
 
--- 2024/day-02/src/parser.rs
+++ 2024/day-02/src/parser.rs
@@ -1,5 +1,9 @@
 use anyhow::Result;
 
-pub fn parse(_input: &str) -> Result<()> {
-    todo!()
+pub fn parse(input: &str) -> impl Iterator<Item = Result<Vec<u64>>> + '_ {
+    input.lines().map(|l| {
+        l.split_ascii_whitespace()
+            .map(|level| Ok(level.parse()?))
+            .collect()
+    })
 }
