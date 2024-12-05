509775f5a36270e7d13c99f02df2c7d1cdbe04b2
Mauro Sambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2024-12-05 09:57:25 -0300

    day-05 - start

---
+++ 2024/day-05/src/bin/part1.rs
@@ -0,0 +1,64 @@
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let input = include_str!("../../input.txt");
+
+    let result = process(input)?;
+
+    println!("{result}");
+
+    Ok(())
+}
+
+#[inline]
+fn process(_input: &str) -> Result<u64> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    #![allow(unused)]
+    use super::*;
+
+    const INPUT: &str = "
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
+    ";
+
+    #[test]
+    fn test_example() -> Result<()> {
+        let expected: u64 = todo!();
+
+        let result = process(INPUT)?;
+
+        assert_eq!(expected, result);
+
+        Ok(())
+    }
+}
